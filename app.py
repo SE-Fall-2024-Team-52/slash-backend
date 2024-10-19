d57ddb45c3297d0ce5e1df9a9be88b9c9fc4196c
Shubham <shubham.kakirde@spit.ac.in>
2024-10-19 13:15:00 -0400

    added seller side api

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ from util.hashing import get_hashed_password, verify_password
 from sqlalchemy.orm import Session
 import database.models as models
 from flask_cors import CORS
+from datetime import datetime
+
 
 app = Flask(__name__)
 CORS(app)
@@ -12,8 +14,78 @@ CORS(app)
 
 
 @app.route("/", methods=["GET", "POST"])
-def hello(request):
+def hello():
     return get_hashed_password("password", "username")
+
+
+@app.route("/postings/<username>", methods=["GET"])
+def get_postings(username):
+    validation1 = (
+        db_session.query(models.Users).filter(models.Users.username == username).first()
+    )
+    if validation1 is None:
+        return jsonify(
+            content={"status": "error", "message": f"User {username} does not exist"}
+        )
+    products = (
+        db_session.query(models.ProductPostings)
+        .filter(models.ProductPostings.posted_by == validation1.id)
+        .all()
+    )
+    return jsonify(
+        content={
+            "status": "success",
+            "data": [
+                {
+                    "name": product.name,
+                    "date_posted": product.date_posted,
+                    "description": product.description,
+                    "price": product.price,
+                    "currency": product.currency,
+                    "sold": product.sold,
+                    "posted_by": f"{validation1.first_name} {validation1.last_name}",
+                }
+                for product in products
+            ],
+        }
+    )
+
+
+@app.route("/add-posting", methods=["POST"])
+def add_posting():
+
+    name = request.json.get("name")
+    description = request.json.get("description")
+    price = request.json.get("price")
+    currency = request.json.get("currency")
+    username = request.json.get("username")
+    today_date = datetime.today().strftime("%Y-%m-%d")
+
+    validation1 = (
+        db_session.query(models.Users).filter(models.Users.username == username).first()
+    )
+    if validation1 is None:
+        return jsonify(
+            content={"status": "error", "message": f"User {username} does not exist"}
+        )
+
+    new_product = models.ProductPostings(
+        name=name,
+        description=description,
+        price=price,
+        currency=currency,
+        date_posted=today_date,
+        posted_by=validation1.id,
+        sold=False,
+    )
+    db_session.add(new_product)
+    db_session.commit()
+
+    return jsonify(
+        content={
+            "status": "success",
+        }
+    )
 
 
 @app.route("/login", methods=["POST"])
@@ -59,11 +131,11 @@ def register_user():
     password = request.json.get("password")
 
     validation1 = (
-        db_session.query(models.Users).filter(models.Users.username == "some").first()
+        db_session.query(models.Users).filter(models.Users.username == username).first()
     )
 
     validation2 = (
-        db_session.query(models.Users).filter(models.Users.email == "some").first()
+        db_session.query(models.Users).filter(models.Users.email == email).first()
     )
 
     if validation1 is not None:
