3a62ef6f23365726eb950469263e21bd5a47019a
Rakshita Tantry <28rakshi@gmail.com>
2024-11-01 22:21:42 -0400

    fixed issue

--- tests/test_api_bestbuy.py
+++ tests/test_api_bestbuy.py
@@ -1,16 +1,10 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Nov 30 00:03:30 2021
-
-@author: Rohan Shah
-"""
 import os
 import sys
 import inspect
 currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
 parentdir = os.path.dirname(currentdir)
 sys.path.insert(0, parentdir)
-from ..app import search_items_API
+from app import search_items_API
 
 def test_api_bestbuy():
     product = 'laptop'
